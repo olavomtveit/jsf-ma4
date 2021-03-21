ec5408f8f5b65108fc97cea5506db8dc4d77d476
olavomtveit <omtveit94@gmail.com>
2021-03-21 13:16:10 +0100

    potential bug fix #2

--- pages/breweries/[slug].js
+++ pages/breweries/[slug].js
@@ -1,4 +1,4 @@
-import Layout from "../../component/Layout/Layout";
+import Layout from "../../components/Layout/Layout";
 import { BASE_URL } from "../../constants/api";
 import axios from "axios";
 
